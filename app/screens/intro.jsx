1d00347a3bfbd7bf83165fc8fab304312115f50b
patrickbasham <PatrickTBasham@gmail.com>
2022-09-04 03:25:36 -0400

    created intro.jsx screen

---
+++ app/screens/intro.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export const intro = () => {
+  return (
+    <View>
+      <Text>Enter Your Name to Contunue</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
