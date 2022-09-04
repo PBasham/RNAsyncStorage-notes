bceba6661fa0e73c7f4f91c41289fd10b51ba50a
patrickbasham <PatrickTBasham@gmail.com>
2022-09-04 16:30:08 -0400

    added two buttons to note detail for delete and edit, not yet working

--- app/components/NoteDetail.jsx
+++ app/components/NoteDetail.jsx
@@ -4,6 +4,7 @@
 import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import { useHeaderHeight } from "@react-navigation/elements"
 import colors from "../misc/colors"
+import { RoundIconBtn } from "./RoundIconBtn"
 
 export const NoteDetail = (props) => {
 
@@ -35,6 +36,16 @@ export const NoteDetail = (props) => {
             <Text style={styles.time} >{`Created at ${formatDate(note.time)}`}</Text>
             <Text style={styles.title} >{note.title}</Text>
             <Text style={styles.desc} >{note.desc}</Text>
+
+            <View style={styles.btnContainer}>
+                <RoundIconBtn antIconName="delete" style={{
+                    backgroundColor: colors.ERROR,
+                    marginBottom: 15,
+                }}
+                onPress={() => console.log("deleting note")}
+                />
+                <RoundIconBtn antIconName="edit" />
+            </View>
         </ScrollView>
     )
 }
@@ -44,7 +55,7 @@ export const NoteDetail = (props) => {
 ========================================*/
 const styles = StyleSheet.create({
     container: {
-        // flex: 1,
+        flex: 1,
         paddingHorizontal: 15,
     },
     title: {
@@ -60,5 +71,10 @@ const styles = StyleSheet.create({
         textAlign: "right",
         fontSize: 16,
         opacity: 0.5,
+    },
+    btnContainer: {
+        position: "absolute",
+        right: 50,
+        bottom: 50,
     }
 })
