61c73f26a3699bfc84ebc165b8469af44662e480
patrickbasham <PatrickTBasham@gmail.com>
2022-09-04 16:02:35 -0400

    updated styling for note detail component

--- app/components/NoteDetail.jsx
+++ app/components/NoteDetail.jsx
@@ -1,15 +1,20 @@
+/*========================================
+        Import Dependencies
+========================================*/
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { useHeaderHeight } from "@react-navigation/elements"
 
 export const NoteDetail = (props) => {
     const { note } = props.route.params
-
+    const headerHeight = useHeaderHeight()
   return (
-    <View>
+    <View style={[styles.container, {paddingTop: headerHeight}]}>
       <Text>{ note.title }</Text>
       <Text>{ note.desc }</Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+})
